5327030ff1b96714006c271e52f5dfbd708e23b3
Amin Solkar <aminsolkar5@gmail.com>
2024-10-15 20:36:52 +0530

    Commit before merge

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function CTA({content}) {
     return (
-            <button className='rounded-2xl bg-red-600 px-4 font-light h-[60%] duration-100 hover:scale-105'>
-                {content}
-            </button> 
+        <button className='rounded-2xl bg-red-600 px-4 font-light h-[60%] duration-100 hover:scale-105'>
+            {content}
+        </button> 
     )
 }
 
